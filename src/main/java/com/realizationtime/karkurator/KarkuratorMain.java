ab25b1c499f9f283e4331c88df1b1bc69bb46a20
Bartłomiej Wręga <bwrega@gmail.com>
2020-05-30 19:53:34 +0200

    grid pane as main layout

--- src/main/java/com/realizationtime/karkurator/KarkuratorMain.java
+++ src/main/java/com/realizationtime/karkurator/KarkuratorMain.java
@@ -3,14 +3,14 @@ package com.realizationtime.karkurator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class KarkuratorMain extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    SplitPane mainPanel = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
+    GridPane mainPanel = FXMLLoader.load(getClass().getResource("/MainView.fxml"));
     Scene scene = new Scene(mainPanel);
     primaryStage.setTitle("Karkurator");
     primaryStage.setScene(scene);
